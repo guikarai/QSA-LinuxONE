5de81ebdd1d1b46874e46e6f086d9710cff0b396
Guillaume HOAREAU <guikarai@yahoo.fr>
2025-02-22 15:07:15 +0100

    Create 2-API-JAVA.sh

---
+++ 2-API-JAVA.sh
@@ -0,0 +1,17 @@
+# ***************************************************************************************************
+# QUANTUM-SAFE ON IBM Z AND LINUXONE WITH IBM
+# AUTHOR: GUILLAUME HOAREAU
+# PURPOSE OF THE FOLLOWING SCRIPT IS TO LIST IN A FILE JAVA API CONFIGURATION ATTRIBUTES OF A HOST
+# 1-INFRA
+# ****************************************************************************************************
+
+#!/bin/bash
+
+#Verify the installed Java version to ensure JCE is available.
+java -version > "2-api-java-version-$(hostname)-$(date +%Y-%m-%d).txt"
+
+#If your Java application uses SSL/TLS, validate its configuration with the following command.
+java -Djavax.net.debug=ssl -version > "2-api-java-ssltls-version-$(hostname)-$(date +%Y-%m-%d).txt"
+
+#Display the installed security providers and their configuration.
+java -Djava.security.debug=provider -version > "2-api-java-security-providers-$(hostname)-$(date +%Y-%m-%d).txt"
