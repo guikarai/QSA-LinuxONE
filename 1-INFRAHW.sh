50dda0fb158680461c0d4eb3aa878f50efb4dde5
Guillaume HOAREAU <guikarai@yahoo.fr>
2025-02-22 15:00:01 +0100

    Update 1-INFRAHW.sh

--- 1-INFRAHW.sh
+++ 1-INFRAHW.sh
@@ -1,6 +1,11 @@
-##################################################################
-##
-##
+# *******************************************************************************************
+# QUANTUM-SAFE ON IBM Z AND LINUXONE WITH IBM
+# AUTHOR: GUILLAUME HOAREAU
+# PURPOSE OF THE FOLLOWING SCRIPT IS TO LIST IN A FILE HW CONFIGURATION ATTRIBUTES OF A HOST
+# 1-INFRA
+# *******************************************************************************************
+
+#!/bin/bash
 
 #The command lscpu displays information on processor architecture.
 lscpu > "1-infra-config-lscpu-$(hostname)-$(date +%Y-%m-%d).txt"
